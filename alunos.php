bae92fbc0176e41d06b161068605b9c4fcd7fa20
Wendel Duarte <105465397+Wendeldev87@users.noreply.github.com>
2022-07-18 20:12:49 -0300

    Update alunos.php

--- alunos.php
+++ alunos.php
@@ -1,26 +1,32 @@
 <?php
 
-$alunos = [
-  [
-    "nome" => "João",
-    "nota" => 7.5
-  ],
-  [
-    "nome" => "Maria",
-    "nota" => 9.5
-  ],
-  [
-    "nome" => "Pedro",
-    "notas" => [8.5, 2, 3]
-  ],
-  [
-    "nome" => "Ana",
-    "nota" => 6.5
-  ],
-  [
-    "nome" => "José",
-    "nota" => 10
-  ]
+$a1 = [
+    'nome' => 'Chiquim',
+    'email' => 'Chiquim@email.com',
+    'telefone' => '85 9 8889-0000',
+    'notas' => [
+        9,
+        8.9
+        7
+        6
+    ]
 ];
 
-var_dump($alunos);
+$a2 = [ 
+    'nome' => '',
+    'email' => 'maria@email.com',
+    'telefone' => '85 9 9999-0000', 
+    'notas' => [
+        9,
+        8.9
+        7
+        6
+    ]
+];
+
+$alunos = [ //dimensao 1
+    $a1,
+    $a2,
+];
+
+$var_dump($alunos);
