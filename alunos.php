de56ffed7afe6743eb9ac8a14c579990b1290163
Wendel Duarte <105465397+Wendeldev87@users.noreply.github.com>
2022-07-18 22:50:08 -0300

    Update alunos.php

--- alunos.php
+++ alunos.php
@@ -7,7 +7,7 @@ $a1 = [
     'notas' => [
         9,
         8.9
-        7
+        7 
         6
     ]
 ];
@@ -24,9 +24,67 @@ $a2 = [
     ]
 ];
 
+$a3 = [ 
+    'nome' => 'Zezim',
+    'email' => 'zezim@email.com',
+    'telefone' => '85 9 9999-0000', 
+    'notas' => [
+        9,
+        8.9
+        7
+        6
+    ]
+];
+
+$a4 = [ 
+    'nome' => 'Luiza',
+    'email' => 'luiza@email.com',
+    'telefone' => '85 9 9999-0000', 
+    'notas' => [
+        9,
+        8.9
+        7
+        6
+    ]
+];
+
+
 $alunos = [ //dimensao 1
     $a1,
     $a2,
 ];
+ 
+foreach ($alunos as $cadaAluno) {
+    echo ($cadaAluno)['nome']. '<br>';
+}
 
-$var_dump($alunos);
+?>
+
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+<div class="container">
+    <h1 class="mt-5> Alunos </h1">
+    <hr>
+<table class="table table-hover table striped" mt-5">
+    <thead class="table-dark">
+        <tr>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Telefone</th>
+        </tr>
+</thread>
+</tbody>
+    <?php
+      foreach ($alunos as $cadaAluno) {
+        echo '<tr>';
+            echo '<td>' . $cadaAluno['nome'] . '</td>';
+            echo '<td>' . $cadaAluno['email'] . '</td';
+            echo '<td>' . $cadaAluno['$cadaAluno']
+
+        echo ($cadaAluno)['nome']. '<br>';  
+      }
+    ?>
+</tbody>
+</table>
+</div>
